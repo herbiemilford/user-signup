02dc2d152a1b6bd640094c76d4bff225ee784746
Joe Crowder <toast.one.art@gmail.com>
2018-10-21 15:48:26 -0500

    add form and base template

--- main.py
+++ main.py
@@ -4,7 +4,29 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+form = '''<form action="/signup" method="POST">
+   <label for ="username">Username
+          <input type="text" name="username" value="{{username}}" />
+          <p>{{username_error}}</p>
+</label>
 
+<label for="password">Password
+          <input type="password" name="password" value="{{password}}" />
+          <p>{{password_error}}</p>
+<label for = "verify>Verify Password
+          <input type="password" name="verify" value="{{verify}}" />
+          <p>{{verify_error}}</p>
+</label>
+<label for="email">Email
+          <input type="text" name="email" value="{{email}}" />
+          <p>{{email_error}}</p>
+<label>
+          <input type="submit" name="submit" value="Submit">
+</label>
+        </form>
+
+
+'''
 @app.route("/")
 def index():
     return form
