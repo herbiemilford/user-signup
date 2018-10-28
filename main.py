b4e9e5b2b33db9f72c355fbe0fb0e8518f757a2a
Joe Crowder <toast.one.art@gmail.com>
2018-10-28 11:27:41 -0500

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, redirect, render_template
+import cgi
+import os
+import re
 
 
 
@@ -64,4 +67,24 @@ def signup():
 
     if verify == "" or verify != password: #verify password
         verify_error = "Passwords do not match. Try again."
+        verify = ""
+
+    if email != "": #validate email try regex bonus
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            email_error = "Not a valid email address." # and not email_error:
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template('welcome.html', username = username)
+    else:
+        return render_template(
+            'index.html',
+            username = username,
+            username_error = username_error,
+            password_error = password_error,
+            verify_error = verify_error,
+            email = email,
+            email_error = email_error
+            )
+
+
+    
 app.run()
