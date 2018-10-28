5611f0aa59f958d2e1a35f9673b6337bf9fc4f2a
Joe Crowder <toast.one.art@gmail.com>
2018-10-28 11:32:32 -0500

    welcome template

--- main.py
+++ main.py
@@ -7,29 +7,8 @@ import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-form = '''<form action="/signup" method="POST">
-   <label for ="username">Username
-          <input type="text" name="username" value=" " />
-          <p></p>
-</label>
-
-<label for="password">Password
-          <input type="password" name="password" value="" />
-          <p></p>
-<label for = "verify">Verify Password
-          <input type="password" name="verify" value="" />
-          <p></p>
-</label>
-<label for="email">Email
-          <input type="text" name="email" value=" " />
-          <p></p>
-<label>
-          <input type="submit" name="submit" value="Submit">
-</label>
-        </form>
 
 
-'''
 @app.route("/")
 def index():
     return render_template('index.html', title = "Sign UP")
