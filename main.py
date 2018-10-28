617f64dae673ac70a07732f38e3f8d41e1e96091
Joe Crowder <toast.one.art@gmail.com>
2018-10-28 10:47:32 -0500

    added templates and base form

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 form = '''<form action="/signup" method="POST">
    <label for ="username">Username
-          <input type="text" name="username" value="{{username}}" />
-          <p>{{username_error}}</p>
+          <input type="text" name="username" value=" " />
+          <p></p>
 </label>
 
 <label for="password">Password
-          <input type="password" name="password" value="{{password}}" />
-          <p>{{password_error}}</p>
-<label for = "verify>Verify Password
-          <input type="password" name="verify" value="{{verify}}" />
-          <p>{{verify_error}}</p>
+          <input type="password" name="password" value="" />
+          <p></p>
+<label for = "verify">Verify Password
+          <input type="password" name="verify" value="" />
+          <p></p>
 </label>
 <label for="email">Email
-          <input type="text" name="email" value="{{email}}" />
-          <p>{{email_error}}</p>
+          <input type="text" name="email" value=" " />
+          <p></p>
 <label>
           <input type="submit" name="submit" value="Submit">
 </label>
