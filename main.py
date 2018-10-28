d27fc1f932f238c2918d86bc7d855bc81d565231
Joe Crowder <toast.one.art@gmail.com>
2018-10-28 11:12:19 -0500

    created functions for username, password, verify

--- main.py
+++ main.py
@@ -29,6 +29,39 @@ form = '''<form action="/signup" method="POST">
 '''
 @app.route("/")
 def index():
-    return form
+    return render_template('index.html', title = "Sign UP")
+ 
 
+@app.route("/signup", methods = ['POST'])
+def signup():
+
+    username = request.form ["username"]
+    password = request.form["password"]
+    verify = request.form["verify"]
+    email = request.form["email"]
+
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
+
+    if username == "": #validate username
+        username_error = "Enter a valid username."
+    elif len(username) <= 3 or len(username) > 20:
+        username_error = "Username must be between 3 and 20 characters in length."
+        username = ""
+    elif " " in username:
+        username_error = "Username cannot contain spaces."
+        username = ""
+
+
+    if password == "":  #validate password
+        password_error = "Enter a valid password."
+    elif len(password) < 3 or len(password) > 20:
+        password_error = "Password must be between 3 and 20 characters in length."
+    elif " " in password:
+        password_error = "Password cannot contain spaces."
+
+    if verify == "" or verify != password: #verify password
+        verify_error = "Passwords do not match. Try again."
 app.run()
